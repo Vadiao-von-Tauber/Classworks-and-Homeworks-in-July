88f7f63f25eb5ef27e7369191c6a3fa06382ae6e
501punkofthedump <darkvadish@gmail.com>
2023-07-20 00:21:44 +0300

    Homework at 19/07, part one.

---
+++ Homework_FileWork_II.py
@@ -0,0 +1,27 @@
+# Upload Homework_FileWork
+
+import Homework_FileWork
+
+class WarpStreams:
+    @staticmethod
+    def warp_streams():
+        pass
+
+    @staticmethod
+    def interworld_bases():
+        pass
+
+    @staticmethod
+    def navis_nobilitae():
+        pass
+
+
+list_ships = ['I. starship Cruor Vult.txt',
+              'II. battleship Alexei Stukov',
+              'III. cargoship Black Jenna',
+              'IV. tugboat Jurgen',
+              'V. space yacht Externus Exterminatus']
+print(list_ships)
+len(list_ships)
+print('\n \n \n immaterium \n      is a \n          dangerous')
+print()
