fccf5f8ee139c068946c7f5211030809e2d37da4
501punkofthedump <darkvadish@gmail.com>
2023-07-19 17:17:20 +0300

    Homework at 19/07, part one.

--- Homework_FileWork.py
+++ Homework_FileWork.py
@@ -49,3 +49,43 @@ class Starships_Info:
 
 
 Starships_Info.star_port_order()
+
+# .txt docs:
+#
+# I. starship Cruor Vult.txt'
+# name ship: "Crvor Vult"
+# codename: "carmine"
+# space displacement: 800000 brt
+# number of guns: 104
+# hold volume: 380000 brt
+# home port: "Koronus Prime", 18004458900501
+# status: 'in immaterium'.
+
+
+
+# ___
+
+# II. battleship Alexei Stukov.txt
+# name ship: "Alexei Stukov"
+# codename: "mare sancti"
+# space displacement: 500000 brt
+# number of guns: 384
+# hold volume: 135000 brt
+# home port: "Mar Sara", 204600451-000DTR02
+# status: 'loading ammunition'
+
+
+# ___
+
+# III. cargoship Black Jenna.txt
+# name ship: "Black Jenna"
+# codename: "obsidian moon"
+# space displacement: 2000000 brt
+# number of guns: 32
+# hold volume: 1800000 brt
+# home port: "Serendipity", 365444200006
+# status: 'in immaterium'.
+
+
+# ___
+
